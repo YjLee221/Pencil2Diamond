21165bd3aa2f90f7a9cc711b0d6c934c45ec364b
LYJ <yjsh221@gmail.com>
2026-04-01 21:44:08 +0900

    feat: 미니게임 패널 전환 로직 및 연필/칼 에셋 추가
    1. 미니 게임으로의 진입 및 퇴장을 위한 버튼 함수 구현
    2. 단계별 연필 깎기 스프라이트 및 칼 에셋 리소스 추가

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -1,20 +1,53 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("UI Item")]
     [SerializeField] Button btnKnife;
     [SerializeField] Button btnPencilCutter;
 
-    public void OnClickKnife()
+    [Header("UI Text")]
+    [SerializeField] TextMeshProUGUI uiText;
+    [SerializeField] Button btnConfirm;
+    [SerializeField] Button btnCancel;
+
+    public void Start()
     {
-        Debug.Log("Knife Clicked");
-        btnPencilCutter.gameObject.SetActive(false);
+        btnConfirm.gameObject.SetActive(false);
+        btnCancel.gameObject.SetActive(false);
     }
 
-    public void OnClickPencilCutter()
+    public void OnClickItem(Button clickedButton)
     {
-        Debug.Log("Pencil Cutter Clicked");
+        // 클릭된 버튼이 아닌 다른 아이템 버튼을 비활성화
+        Button otherButton = clickedButton == btnKnife ? btnPencilCutter : btnKnife;
+        otherButton.gameObject.SetActive(false);
+        
+        OnClickTool();
+    }
+
+    void OnClickTool()
+    {
+        uiText.gameObject.SetActive(false);
+        btnConfirm.gameObject.SetActive(true);
+        btnCancel.gameObject.SetActive(true);
+    }
+
+    public void OnClickConfirm()
+    {
+
+    }
+
+    public void OnClickCancel()
+    {
+        btnConfirm.gameObject.SetActive(false);
+        btnCancel.gameObject.SetActive(false);
+
+        uiText.gameObject.SetActive(true);
         btnKnife.gameObject.SetActive(true);
+        btnPencilCutter.gameObject.SetActive(true);
     }
 }
